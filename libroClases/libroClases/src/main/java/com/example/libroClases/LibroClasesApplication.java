7b0305c2d6f8d8e712302e98609a983ae823e655
michaelciudad <mi.ciudad@duocuc.cl>
2026-06-17 14:04:48 -0400

    Add files via upload

---
+++ libroClases/libroClases/src/main/java/com/example/libroClases/LibroClasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.libroClases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroClasesApplication.class, args);
+	}
+
+}
---
+++ libroClases/libroClases/src/test/java/com/example/libroClases/LibroClasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libroClases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroClasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
